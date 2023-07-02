070c1e56bd38edc81264450ac9330419061b056e
sergio portillo <sergiotello83@hotmail.com>
2023-07-02 14:25:37 -0500

    implementacion de snackbars

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -1,15 +1,37 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class SnackbarScreen extends StatelessWidget {
   
   static const name = 'snackbar_screen';
 
   const SnackbarScreen({super.key});
+
+  void showSnackBar(BuildContext context){
+
+    ScaffoldMessenger.of(context).clearSnackBars();
+
+    final snackBar =  SnackBar(content: const Text('Hello world!'),
+    action: SnackBarAction(label: 'OK', onPressed: (){}),
+    duration: const Duration(seconds: 2),
+    );
+
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+  }
   
   @override
   Widget build(BuildContext context) {
 
-      return const Placeholder();
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Snackbars'),
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () => showSnackBar(context),
+          label: const Text('Mostrar snackbar'),
+          icon: const Icon(Icons.remove_red_eye_outlined),
+          ),
+      );
   }
   
 }
