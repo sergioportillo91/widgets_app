0279f45b6cdb1c1e4d5dc86b992f59d542de4609
sergio portillo <sergiotello83@hotmail.com>
2023-07-02 12:39:07 -0500

    creacion de ui controls screen

---
+++ lib/presentation/screens/ui_controls/ui_controls.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class UIControlsScreen extends StatelessWidget {
+  
+  static const name = 'ui_controls_screen';
+
+  const UIControlsScreen({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+
+      return const Placeholder();
+  }
+  
+}
