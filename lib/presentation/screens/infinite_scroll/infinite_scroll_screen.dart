63bf6398a80cd5c189bcc1d05a13b32804846396
sergio portillo <sergiotello83@hotmail.com>
2023-07-02 12:38:26 -0500

    creacion de infirnite scroll screen

---
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class InfiniteScrollScreen extends StatelessWidget {
+  
+  static const name = 'infinite_scroll_screen';
+
+  const InfiniteScrollScreen({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+
+      return const Placeholder();
+  }
+  
+}
