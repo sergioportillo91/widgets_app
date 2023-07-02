2f95764e597d0af981170c5ccf5631708d1f4250
sergio portillo <sergiotello83@hotmail.com>
2023-07-02 12:38:39 -0500

    creacion de progress indicatos screen

---
+++ lib/presentation/screens/progress_indicator/progress_indicator_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class ProgressIndicatosScreen extends StatelessWidget {
+  
+  static const name = 'progress_indicator_screen';
+
+  const ProgressIndicatosScreen({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+
+      return const Placeholder();
+  }
+  
+}
