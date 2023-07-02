d3cac5a63cfa8b133d47ff29bbd392a192d5471f
sergio portillo <sergiotello83@hotmail.com>
2023-07-02 12:37:57 -0500

    creacion de app_tutorial-screen

---
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class AppTutorialScreen extends StatelessWidget {
+  
+  static const name = 'tutorial_screen';
+
+  const AppTutorialScreen({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+
+      return const Placeholder();
+  }
+  
+}
