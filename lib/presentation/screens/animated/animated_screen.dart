08f68c5a4c730a24732ea92e742f3a1872c496b4
sergio portillo <sergiotello83@hotmail.com>
2023-07-02 12:37:41 -0500

    creacion de screen animated screen

---
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class AnimatedScreen extends StatelessWidget {
+  
+  static const name = 'animated_screen';
+
+  const AnimatedScreen({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+
+      return const Placeholder();
+  }
+  
+}
