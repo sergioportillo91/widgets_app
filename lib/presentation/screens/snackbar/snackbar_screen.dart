8bd3841136c8bdc43a96e7f9786396b0f7b82e67
sergio portillo <sergiotello83@hotmail.com>
2023-07-02 12:38:54 -0500

    creacion de snackbar screen

---
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class SnackbarScreen extends StatelessWidget {
+  
+  static const name = 'snackbar_screen';
+
+  const SnackbarScreen({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+
+      return const Placeholder();
+  }
+  
+}
